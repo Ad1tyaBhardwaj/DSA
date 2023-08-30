ce83b2a95baf394746d59db1b3cf6d9ab5a08809
Aditya Bhardwaj <adityabhardwaj98144@gmail.com>
2023-08-31 02:11:57 +0530

    Try1

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -18,8 +18,25 @@ public class BinarySearch {
         return -1;
     }
 
+    public static int RbSearch(int arr[], int low, int high, int x){
+        if (low>high){
+            return -1;
+        }
+        int mid = (low+high)/2;
+        if(arr[mid]==x){
+            return mid;
+        }
+        else if(arr[mid]>x){
+            return RbSearch(arr,low,mid-1,x);
+        }
+        else{
+            return RbSearch(arr,mid+1,high,x);
+        }
+    }
+
     public static void main(String[] args) {
         int[] temp = {10,20,30,40,50};
         System.out.println(bSearch(temp,25));
+        System.out.println(RbSearch(temp,0,4,30));
     }
 }
