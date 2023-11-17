88c0a6e72bf4d5d1527710d22d33cfdf3a44f4e7
Aditya Bhardwaj <adityabhardwaj98144@gmail.com>
2023-11-17 22:46:26 +0530

    Try1

---
+++ src/Knapsack01.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Knapsack01 {
+
+    public static int knapsack(int wt[], int val[], int W, int n){
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> emp = new ArrayList<>();
+        emp.add(3);
+        emp.add(4);
+        emp.get(0);
+    }
+}
