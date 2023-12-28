2a55accb9b1cd33d50768db4cd5769cea0fc9157
Aditya Bhardwaj <adityabhardwaj98144@gmail.com>
2023-12-28 13:08:22 +0530

    Try1

--- src/trmpo.java
+++ src/trmpo.java
@@ -1,19 +1,44 @@
-public class trmpo {
-    public static int getDecimalValue(ListNode head) {
-        String str = "";
-        while (head.next != null) {
-            str = str + head.val;
-            head = head.next;
-        }
+import java.util.ArrayList;
 
-        return Integer.parseInt(str, 2);
+public class trmpo {
+    public static void main(String[] args){
+        ArrayList<Integer> hello = new ArrayList<>();
+        hello.add(1);
+        hello.add(2);
+
+        while(hello.isEmpty()){
+            System.out.println(hello.remove(0));
+        }
     }
 
-    public static void main(String[] args) {
-        String temp = "";
-        temp = temp+2;
-        temp = temp+3;
-        System.out.println(temp);
-        System.out.println(Integer.parseInt(temp,2));
+    public int[][] asliFunc(int[][] matrix, int x, int y){
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(i == x || j == y){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        return matrix;
+    }
+
+    public void setZeroes(int[][] matrix) {
+        ArrayList<ArrayList<Integer>> val = new ArrayList<>();
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(matrix[i][j] == 0){
+                    ArrayList<Integer> temp = new ArrayList<>();
+                    temp.add(i);
+                    temp.add(j);
+                    val.add(temp);
+                }
+            }
+        }
+
+        while(!val.isEmpty()){
+            ArrayList<Integer> tempo = val.remove(0);
+            tempo.get(0);
+        }
+        return matrix;
     }
 }
